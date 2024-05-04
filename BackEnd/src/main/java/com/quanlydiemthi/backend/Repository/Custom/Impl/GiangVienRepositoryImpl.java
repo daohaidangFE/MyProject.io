97a481791db90f1bc76520f227a94d5c13ea0482
Dao Hai Dang <daohaidangpc@gmail.com>
2024-05-04 15:56:40 +0700

    đang chưa tìm kiếm theo trạng thái được

--- BackEnd/src/main/java/com/quanlydiemthi/backend/Repository/Custom/Impl/GiangVienRepositoryImpl.java
+++ BackEnd/src/main/java/com/quanlydiemthi/backend/Repository/Custom/Impl/GiangVienRepositoryImpl.java
@@ -18,7 +18,7 @@ public class GiangVienRepositoryImpl implements GiangVienRepositoryCustom {
             for (Map.Entry<String, String> entry : conditions.entrySet()) {
                 String fieldName = entry.getKey();
                 String value = entry.getValue().replaceAll("\s\s+", " ").trim();
-                if ("magv".equals(fieldName) || "username".equals(fieldName) || "gioi_tinh".equals(fieldName) || "tengv".equals(fieldName)) {
+                if ("magv".equals(fieldName) || "username".equals(fieldName) || "gioi_tinh".equals(fieldName) || "tengv".equals(fieldName) || "is_active".equals(fieldName)) {
                     if (value != null && !value.isEmpty()) {
                         sql.append("AND gv.").append(fieldName).append(" LIKE '%").append(value).append("%' ");
                     }
